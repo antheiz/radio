64f6b9aaf0cb2e167b890318fd1dd5438fed7786
antheiz <45439030+antheiz@users.noreply.github.com>
2025-04-28 21:32:07 +0900

    fix: update base url with new sub domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://antheiz.github.io/',
-  base: '/radio/',
+  site: 'https://radio.antheiz.space/',
   integrations: [tailwind(), svelte()],
 });
